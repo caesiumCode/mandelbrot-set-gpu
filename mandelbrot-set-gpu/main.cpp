bab68f4eb45c0140e211c90e3c8203f260e58bf0
caesiumCode <caesiumcode@aol.com>
2021-07-03 10:23:08 +0200

    Initial Commit

---
+++ mandelbrot-set-gpu/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  mandelbrot-set-gpu
+//
+//  Created by Stephen Jaud on 03/07/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
